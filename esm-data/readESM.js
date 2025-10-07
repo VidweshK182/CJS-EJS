1978db79624973173506e75c2fe82f17f1283ace
Kadarla Sri Vidwesh <vidweshkadarla182@gmail.com>
2025-10-07 14:55:50 +0530

    esm with json

---
+++ esm-data/readESM.js
@@ -0,0 +1,4 @@
+import data  from  './data.json' with {type :"json"}
+console.log("course: " + data.course);
+console.log("instructor: "+ data.instructor);
+console.log("students: " + data.students)
